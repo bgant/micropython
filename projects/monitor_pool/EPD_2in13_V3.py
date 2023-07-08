4a44d1d6e309c264c9747dcb55fdbcb9d61496e0
Brandon Gant <bgant@protonmail.com>
2023-07-08 15:32:07 -0500

    Pool Monitor in Micropython

--- projects/monitor_pool/EPD_2in13_V3.py
+++ projects/monitor_pool/EPD_2in13_V3.py
@@ -276,7 +276,7 @@ class EPD_2in13_V3_Portrait(framebuf.FrameBuffer):
     parameter:
     '''
     def init(self):
-        print('Display init...')
+        print('Wake Display...')
         self.reset()
         self.delay_ms(100)
         
@@ -513,7 +513,7 @@ class EPD_2in13_V3_Landscape(framebuf.FrameBuffer):
         self.send_data((Ystart >> 8) & 0xFF)
 
     def init(self):
-        print('Display init...')
+        print('Wake Display...')
         self.reset()
         self.delay_ms(100)
         
--- projects/monitor_pool/main.py
+++ projects/monitor_pool/main.py
@@ -1,12 +1,135 @@
+''''
+Brandon Gant
+Created: 2023-05-06
+Updated: 2023-05-08
+
+Parts List:
+    * Unexpected Maker TinyS3 (ESP32-S3 Board)
+      https://www.amazon.com/dp/B09X259SDP
+
+    * Waveshare 2.13in E-Ink Display HAT
+      https://www.amazon.com/dp/B07P9T64BK (Pico_ePaper-2.13_V3.py   Black/White)
+      https://www.amazon.com/dp/B07Q22WDB9 (Pico_ePaper-2.13-B_V4.py Black/White/Red)
+
+    * Adafruit Universal Thermocouple Amplifier MAX31856 Breakout
+      https://www.adafruit.com/product/3263
+      https://www.amazon.com/dp/B01LZBBI7D
+
+    * Waterproof Stainless Steel K-Type Thermocouple
+      https://www.amazon.com/dp/B07PNTDFGJ
+
+    * qBoxMini DIY IOT Enclosure Kit
+      https://www.amazon.com/dp/B088CRR7KS
+
+    * M2.5 Hex Brass Standoff Assortment Kit
+      https://www.amazon.com/product/B075K3QBMX
+
+TinyS3 to Waveshare Pinout:
+    ESP32          Waveshare
+    SCK      <-->  CLK
+    MO       <-->  DIN
+    SS       <-->  CS
+    IO32     <-->  BUSY
+    IO21     <-->  RST
+    IO22     <-->  DC
+  
+Download Files to Upload to Micropython Device:
+    wget -O EPD_2in13_V3.py https://raw.githubusercontent.com/waveshareteam/Pico_ePaper_Code/main/python/Pico_ePaper-2.13-V3.py
+    wget -O EPD_2in13_B_V4.py https://raw.githubusercontent.com/waveshareteam/Pico_ePaper_Code/main/python/Pico_ePaper-2.13-B_V4.py
+    wget https://raw.githubusercontent.com/peterhinch/micropython-font-to-py/master/writer/writer.py
+    wget -O Arial_50_Numbers.py https://raw.githubusercontent.com/peterhinch/micropython-nano-gui/master/gui/fonts/arial_50.py
+
+Edit EPD_2in13_B_V4.py Module to work with SPI:
+    RST_PIN         = 21
+    DC_PIN          = 22
+    CS_PIN          = 5
+    BUSY_PIN        = 32
+
+Edit EPD_2in13_B_V4.py Module to work with SPI and Writer.py:
+Source: https://www.instructables.com/Waveshare-E-paper-Display-With-Raspberry-Pi-Pico-M/
+    RST_PIN         = 21
+    DC_PIN          = 22
+    CS_PIN          = 5
+    BUSY_PIN        = 32
+    
+  - class EPD_2in13_B_V4:
+  + class EPD_2in13_B_V4(framebuf.FrameBuffer):
+  
+    self.imagered = framebuf.FrameBuffer(self.buffer_red, self.width, self.height, framebuf.MONO_HLSB)
+  + super().__init__(self.buffer_balck, self.width, self.height, framebuf.MONO_HLSB)
+    self.init()
+
+Create Font Subsets to Upload to Micropython Device:
+    wget https://raw.githubusercontent.com/peterhinch/micropython-font-to-py/master/font_to_py.py
+    python3 -m venv .venv
+    source .venv/bin/activate
+    python3 -m pip list
+    python3 -m pip install freetype-py
+    python3 font_to_py.py GothamBlack-25.bdf 25 -x -c 1234567890wateri GothamBlack_25.py
+    python3 font_to_py.py GothamBlack-54.bdf 54 -x -c 1234567890 GothamBlack_54_Numbers.py
+
+Create No Power and Fault Images:
+Source: https://learn.adafruit.com/preparing-graphics-for-e-ink-displays/command-line
+    wget -O eink-2color.png https://cdn-learn.adafruit.com/assets/assets/000/079/735/medium800/eink___epaper_eink-2color.png
+    <search and download image online>
+    <Use Gimp to crop and fill black and white colors>
+    convert no-power.jpg -negate -dither FloydSteinberg -define dither:diffusion-amount=100% -remap eink-2color.png PBM:no-power-large.pbm
+    <Gimp --> Image --> Scale Image... to 100 pixels wide>
+
+Secrets Storage on Device using key_store.py:
+    key_store.set('ssid','')
+    key_store.set('password','')
+    key_store.set('lat','')
+    key_store.set('lon','')
+    key_store.set('appid','<from https://api.openweathermap.org>')
+    key_store.set('server','')
+    key_store.set('port','')  # 8086 or 443
+    key_store.set('database','')
+    key_store.set('measurement','')
+    key_store.set('jwt','<from https://jwt.io/#debugger-io>')
+
+JSON Web Token (jwt)
+    If you enabled authentication in InfluxDB you need
+    to create a JSON Web Token to write to a database:
+
+    https://www.unixtimestamp.com/index.php
+        Create a future Unix Timestamp expiration   
+
+    https://jwt.io/#debugger-io
+        HEADER
+            {
+              "alg": "HS256",
+              "typ": "JWT"
+             }
+        PAYLOAD
+            {
+              "username": "<InfluxDB username with WRITE to DATABASE>",
+              "exp": <Unix Timestamp expiration>
+            }
+        VERIFY SIGNATURE
+            HMACSHA256(
+              base64UrlEncode(header) + "." +
+              base64UrlEncode(payload),
+              <shared secret phrase set in InfluxDB>
+            )
+'''
+
+###################################
+# Built-in Modules: help('modules')
+###################################
 from machine import reset, WDT, Timer
 wdt = WDT(timeout=780000)  # Set 13-minute Hardware Watchdog Timer
-
 from time import sleep
+from esp32 import raw_temperature
+
+
+###################################
+# My Custom Modules on Github
+###################################
 import pool_display
 import pool_wifi
 import pool_thermocouple
 from client_id import client_id
-from esp32 import raw_temperature
 
 
 ###################################
@@ -46,7 +169,13 @@ def main(timer_main):
         pool_wifi.wlan_connect(pool_wifi.ssid_name,pool_wifi.ssid_pass)
         gc.collect()
         air_now   = pool_wifi.download_weather()
-    pool_wifi.send_to_influxdb(water=water_now,cpu=cpu_now)
+    
+    # Send Data to InfluxDB Server:
+    try:
+        pool_wifi.send_to_influxdb(water=water_now,cpu=cpu_now)
+    except Exception as e:
+        print(f'ERROR: Failed to connect to InfluxDB server: {e}')
+        pass
     
     # Update Display:
     water_temp = int(roundTraditional(water_now,0)) if type(water_now) is float else water_now  # str or None
@@ -55,6 +184,7 @@ def main(timer_main):
         print('No Temperature Changes... Skipping Display Update...')
     else:
         pool_display.update(water=water_now, air=air_now, power=power_now)
+    print(f'Memory Free:   {int(gc.mem_free()/1024)}KB')
     print('='*45)
     
     # End of loop cleanup:
