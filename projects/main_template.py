ad26dedba21b7ffc23254a09ba7893906bf8b568
Brandon Gant <bgant@protonmail.com>
2024-04-28 15:06:10 -0500

    updated template

--- projects/main_template.py
+++ projects/main_template.py
@@ -1,10 +1,12 @@
 # Initialize Watchdog Timer
 from machine import reset, WDT, Timer
 wdt = WDT(timeout=600000)  # 10  Minute Hardware Watchdog Timer
+main_interval = 60         # Time in seconds between loops
+state = 'loop'            # while 'loop' or 'timer'
 
 # Load key_store/secrets
 from key_store import KEY_STORE
-secrets = KEY_STORE()
+key_store = KEY_STORE()
 
 # Connect to Wifi and set Clock
 from wifi import WIFI
@@ -12,12 +14,7 @@ wifi = WIFI()
 wifi.connect()
 
 # Import Project specific modules
-from vttouchw import VTTOUCHW
-from OpenWeatherMap import WEATHER
-from AirNowAPI import AQI
-from timezone import tz
 
-main_interval = 300000   # Minutes between Timer loops
 
 class PROJECT:
     def __init__(self):
@@ -28,13 +25,26 @@ class PROJECT:
 
 
 project = PROJECT()
-timer_main = Timer(0)
 
 def timer_function(timer_main):
     project.something()
     wdt.feed()
 
-timer_function(timer_main)  # Initial Run on Boot
-timer_main.init(period=main_interval, callback=timer_function)
-# View Timer value: timer_main.value()   Stop Timer: timer_main.deinit()
+
+###################################
+# Run in timer or loop
+###################################
+if state is 'timer':
+    print('main.py running in Timer')
+    timer_main = Timer(0)
+    timer_function(timer_main)  # Initial Run on Boot
+    timer_main.init(period=main_interval*1000, callback=timer_function)
+    # View Timer value: timer_main.value()   Stop Timer: timer_main.deinit()
+elif state is 'loop':
+    print('main.py running in while True loop (Ctrl+C to REPL)')
+    while True:
+        timer_function(None)
+        sleep_ms(main_interval*1000)
+else:
+    print(f'Unknown State: {state}')
 
