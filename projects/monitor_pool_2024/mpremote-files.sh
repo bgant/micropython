7735bdc7446e9ffa1b0fee68b5348aa2d83eebd6
Brandon Gant <bgant@protonmail.com>
2024-04-29 14:02:29 -0500

    file installs work

--- projects/monitor_pool_2024/mpremote-files.sh
+++ projects/monitor_pool_2024/mpremote-files.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
+mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024
+
 mpremote a0 mip install --target= github:bgant/micropython/modules/key_store.py
 mpremote a0 mip install --target= github:bgant/micropython/modules/wifi.py
 
 mpremote a0 mip install --target= github:peterhinch/micropython-font-to-py/writer/writer.py
 mpremote a0 mip install --target= github:eliotb/micropython-max31856/max31856.py
 
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/main.py
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/epaper.py
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/thermocouple.py
-
-#mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/Pico_ePaper_2in13_V3.py
-#mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/Pico_ePaper_2in13_V4.py
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/Pico_ePaper_2in13_B_V4.py
-
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/GothamBlack_54_Numbers.py
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/GothamBlack_25.py
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/GothamBlack_46_Numbers.py
-
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/graphic_fault_100px.pbm
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/graphic_no_power_100px.pbm
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/graphic_no_wifi_100px.pbm
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024/graphic_pool_122px.pbm
