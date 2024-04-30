04385ca3cbfe3c22afab8f7b604e4041ddff407d
Brandon Gant <bgant@protonmail.com>
2024-04-30 16:42:18 -0500

    testing out aiorepl with asyncio

--- projects/monitor_pool_2024/mpremote-install.sh
+++ projects/monitor_pool_2024/mpremote-install.sh
@@ -7,7 +7,10 @@ mpremote a0 mip install --target= github:bgant/micropython/modules/key_store.py
 mpremote a0 mip install --target= github:bgant/micropython/modules/wifi.py
 mpremote a0 mip install --target= github:bgant/micropython/modules/webdis.py
 
+mpremote a0 mip install --target= https://raw.githubusercontent.com/micropython/micropython-lib/master/micropython/aiorepl/aiorepl.py
+
 # Added these into my repo just in case they change significantly:
 #mpremote a0 mip install --target= github:peterhinch/micropython-font-to-py/writer/writer.py
 #mpremote a0 mip install --target= github:eliotb/micropython-max31856/max31856.py
+
 
