b2ee83f8673c88bac6dfa14b7f75462be8b46fb4
Brandon Gant <bgant@protonmail.com>
2024-04-29 20:50:07 -0500

    putting everything together just in case

--- projects/monitor_pool_2024/mpremote-files.sh
+++ projects/monitor_pool_2024/mpremote-files.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024
-
 mpremote a0 mip install --target= github:bgant/micropython/modules/key_store.py
 mpremote a0 mip install --target= github:bgant/micropython/modules/wifi.py
 mpremote a0 mip install --target= github:bgant/micropython/modules/webdis.py
 
-mpremote a0 mip install --target= github:peterhinch/micropython-font-to-py/writer/writer.py
-mpremote a0 mip install --target= github:eliotb/micropython-max31856/max31856.py
+# Added these into my repo just in case they change significantly:
+#mpremote a0 mip install --target= github:peterhinch/micropython-font-to-py/writer/writer.py
+#mpremote a0 mip install --target= github:eliotb/micropython-max31856/max31856.py
 
