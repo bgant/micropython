05ac097bced565120400e41527485dfd3b1d511b
Brandon Gant <bgant@protonmail.com>
2024-04-30 16:40:44 -0500

    improper use of Timer so trying Asyncio

--- projects/monitor_pool_2024/main.py
+++ projects/monitor_pool_2024/main.py
@@ -81,7 +81,6 @@ class PROJECT:
         '''Get water temperature reading from Thermocouple'''
         self.thermocouple.read()
         self.water_now = self.thermocouple.tempF
-        self.send_to_webdis()
     
     def send_to_webdis(self):
         '''Send current water temperature to Webdis'''
@@ -109,24 +108,52 @@ class PROJECT:
         self.air_last   =   self.air_now if not type(self.air_now)   is float else int(self.roundTraditional(self.air_now,0))
         self.power_last = True        
         
-    def loop(self):
-        '''Main Loop'''
+    def tasks(self):
+        '''Main Tasks to Perform'''
         self.check_power()
         self.check_reset()
         self.check_wifi()
         self.water()
+        self.send_to_webdis()
         self.air()
         self.update_display()
         self.cleanup()
+        wdt.feed()
         
 
 project = PROJECT()
 
-def timer_function(timer_main):
-    project.loop()
-    wdt.feed()
-    
 
+import asyncio
+import aiorepl
+
+async def main_loop():
+    while True:
+        project.tasks()
+        await asyncio.sleep(60)
+    
+async def launch_coroutines():
+    print("Launching Asyncio Coroutines...")
+    task1 = asyncio.create_task(main_loop())
+    repl = asyncio.create_task(aiorepl.task())
+    await asyncio.gather(task1,repl)
+
+asyncio.run(launch_coroutines())
+
+
+'''No access to REPL
+import asyncio
+
+async def something():
+    project.loop()
+    await asyncio.sleep(180)
+
+event_loop = asyncio.get_event_loop()
+event_loop.create_task(something())
+event_loop.run_forever()
+'''
+ 
+'''
 ###################################
 # Run in timer or loop
 ###################################
@@ -149,3 +176,4 @@ else:
 # List of variables: dir()
 # List of modules: help('modules')
 # Time Commands: ntp()  time.localtime()  ntptime.settime()
+'''
