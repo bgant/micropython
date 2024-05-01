974fd95410eca7e52f941ca5963d9acd495125a3
Brandon Gant <bgant@protonmail.com>
2024-04-30 21:28:21 -0500

    multiple loop options

--- projects/monitor_pool_2024/main.py
+++ projects/monitor_pool_2024/main.py
@@ -2,7 +2,7 @@
 from machine import reset, WDT, Timer, lightsleep
 wdt = WDT(timeout=600000)  # 10  Minute Hardware Watchdog Timer
 main_interval = 450        # Time in seconds between loops (loop takes 90 seconds)
-state = 'timer'            # while 'loop' or 'timer'
+state = 'thread'           # [ thread | aiorepl | asyncio | timer | loop ]
 
 # Import modules
 from time import sleep_ms, localtime, ticks_ms, ticks_diff
@@ -110,6 +110,7 @@ class PROJECT:
         
     def tasks(self):
         '''Main Tasks to Perform'''
+        print('Begin Main Loop')
         self.check_power()
         self.check_reset()
         self.check_wifi()
@@ -119,72 +120,64 @@ class PROJECT:
         self.update_display()
         self.cleanup()
         wdt.feed()
+        print('End Main Loop')
+        print('='*45)
         
 
 project = PROJECT()
 
-# This seems to be the best way to run loop and access REPL
-import _thread
-def main_loop():
-    while True:
-        project.tasks()
-        sleep_ms(60000)
 
-print('Launching Thread Loop')
-_thread.start_new_thread(main_loop,())
-
-
-'''
-import asyncio
-import aiorepl
-
-async def main_loop():
-    while True:
-        project.tasks()
-        await asyncio.sleep(60)
-    
-async def launch_coroutines():
-    print("Launching Asyncio Coroutines...")
-    task1 = asyncio.create_task(main_loop())
-    repl = asyncio.create_task(aiorepl.task())
-    await asyncio.gather(task1,repl)
-
-asyncio.run(launch_coroutines())
-'''
-
-'''No access to REPL
-import asyncio
-
-async def something():
-    project.loop()
-    await asyncio.sleep(180)
-
-event_loop = asyncio.get_event_loop()
-event_loop.create_task(something())
-event_loop.run_forever()
-'''
- 
-'''
 ###################################
-# Run in timer or loop
+# Run in thread, asyncio, loop, etc
 ###################################
 print()
-if state is 'timer':
+if state is 'thread':
+    import _thread
+    def main_loop():
+        while True:
+            project.tasks()
+            sleep_ms(main_interval*1000)
+    print('Launching Thread Loop')
+    _thread.start_new_thread(main_loop,())
+elif state is 'aiorepl':
+    import asyncio
+    import aiorepl
+    async def main_loop():
+        while True:
+            project.tasks()
+            await asyncio.sleep(main_interval)
+    async def launch_coroutines():
+        print("Launching Asyncio Coroutines...")
+        task1 = asyncio.create_task(main_loop())
+        repl = asyncio.create_task(aiorepl.task())
+        await asyncio.gather(task1,repl)
+    asyncio.run(launch_coroutines())
+elif state is 'asyncio':
+    import asyncio
+    async def main_loop():
+        project.tasks()
+        await asyncio.sleep(main_interval)
+    event_loop = asyncio.get_event_loop()
+    event_loop.create_task(main_loop())
+    event_loop.run_forever()
+elif state is 'timer':
+    def timer_function(timer_main):
+        project.tasks()
     print(f'main.py running in Timer every {main_interval} seconds')
     timer_main = Timer(0)
     timer_function(timer_main)  # Initial Run on Boot
     timer_main.init(period=main_interval*1000, callback=timer_function)
     # View Timer value: timer_main.value()   Stop Timer: timer_main.deinit()
 elif state is 'loop':
+    def main_loop(timer_main):
+        project.tasks()
     print('main.py running in while True loop every {main_interval} seconds (Ctrl+C to REPL)')
     while True:
-        timer_function(None)
+        main_loop()
         sleep_ms(main_interval*1000)
 else:
     print(f'Unknown State: {state}')
 
-
 # List of variables: dir()
 # List of modules: help('modules')
 # Time Commands: ntp()  time.localtime()  ntptime.settime()
-'''
