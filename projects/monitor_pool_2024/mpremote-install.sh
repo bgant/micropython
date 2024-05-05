dae2db36e99e4202e60ce1adc3d1790dc9ea15fc
Brandon Gant <bgant@protonmail.com>
2024-05-05 10:14:27 -0500

    EPD_2in13_V4

--- projects/monitor_pool_2024/mpremote-install.sh
+++ projects/monitor_pool_2024/mpremote-install.sh
@@ -2,15 +2,17 @@
 #
 # This script copies all the files needed for this project onto a TinyPICO or TinyS3.
 # 
-mpremote a0 mip install --target= github:bgant/micropython/projects/monitor_pool_2024
-mpremote a0 mip install --target= github:bgant/micropython/modules/key_store.py
-mpremote a0 mip install --target= github:bgant/micropython/modules/wifi.py
-mpremote a0 mip install --target= github:bgant/micropython/modules/webdis.py
+TTY=a0
 
-mpremote a0 mip install --target= https://raw.githubusercontent.com/micropython/micropython-lib/master/micropython/aiorepl/aiorepl.py
+mpremote $TTY mip install --target= github:bgant/micropython/projects/monitor_pool_2024
+mpremote $TTY mip install --target= github:bgant/micropython/modules/key_store.py
+mpremote $TTY mip install --target= github:bgant/micropython/modules/wifi.py
+mpremote $TTY mip install --target= github:bgant/micropython/modules/webdis.py
+
+mpremote $TTY mip install --target= https://raw.githubusercontent.com/micropython/micropython-lib/master/micropython/aiorepl/aiorepl.py
 
 # Added these into my repo just in case they change significantly:
-#mpremote a0 mip install --target= github:peterhinch/micropython-font-to-py/writer/writer.py
-#mpremote a0 mip install --target= github:eliotb/micropython-max31856/max31856.py
+#mpremote $TTY mip install --target= github:peterhinch/micropython-font-to-py/writer/writer.py
+#mpremote $TTY mip install --target= github:eliotb/micropython-max31856/max31856.py
 
 
