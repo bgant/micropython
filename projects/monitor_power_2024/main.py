dc59d53178fcb7e2d4576cb7e1ce04ab975da1a3
Brandon Gant <bgant@protonmail.com>
2025-01-01 15:31:06 -0600

    Updated and using new ping check

--- projects/monitor_power_2024/main.py
+++ projects/monitor_power_2024/main.py
@@ -4,8 +4,7 @@ wdt = WDT(timeout=180000)  # Set 3-minute Hardware Watchdog Timer
 main_interval = 60         # Time in seconds between Timer loops
 state = 'timer'            # while 'loop' or 'timer'
 
-from key_store import KEY_STORE
-key_store = KEY_STORE()
+import key_store
 
 from wifi import WIFI
 wifi = WIFI()
@@ -14,17 +13,42 @@ wifi.connect()
 from webdis import WEBDIS
 webdis = WEBDIS()
 
+import uping
+
 class PROJECT:
     def __init__(self):
-        self.power = Pin(37, Pin.IN)   # 2.5V Input Pin (any GPIO pin should work)
+        key_store.enable()
+        if key_store.get('sensor_pin'):
+            self.sensor_pin = key_store.get('sensor_pin')
+        else:
+            self.sensor_pin = input('Enter Sensor Pin - ')  # 2.5V Input Pin (any GPIO pin should work)
+            key_store.set('sensor_pin',self.sensor_pin)
+        self.power = Pin(int(self.sensor_pin), Pin.IN)
+
         if key_store.get('webdis_key'):
             self.webdis_key = key_store.get('webdis_key')  # Webdis Key Name
         else:  # key_store value is empty
             self.webdis_key = input('Enter Webdis Key Name - ')
             key_store.set('webdis_key',self.webdis_key)
+        key_store.close()  # Close database and file when finished using it
 
+    def network_fail(self):
+        print('Unable to access network... resetting...')
+        sleep_ms(15000)
+        reset()
+
+    def ping_check(self, target):
+        try:
+            pong = uping.ping(target,quiet=True)
+            if not pong[1]:  # Zero packets received
+                self.network_fail()
+        except:
+            self.network_fail()
+            
     def control(self):
+        self.ping_check(wifi.gateway)
         webdis.timeseries(self.webdis_key,self.power.value())
+        print(webdis.response_text,self.webdis_key,self.power.value())
 
 
 project = PROJECT()
